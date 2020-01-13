211efbb26c98ffbddd9686926b819947d1b5a2be
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-13 20:08:53 +0530

    Update 1_Common_String_Methods.java

--- 1_Common_String_Methods.java
+++ 1_Common_String_Methods.java
@@ -1,7 +1,18 @@
 public class CommonStringMethod {
 
 	public static void main(String[] args) {
+		String words[]= {"funk","chunk","furry","backonator"};
 		
+		
+		//startsWith function in String
+		
+		for(String w : words)
+		{
+			if(w.startsWith("fu"))
+			{
+				System.out.println(w + " Starts with fu");
+			}
+		}
 	}
 
 }
