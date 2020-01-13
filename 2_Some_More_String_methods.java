01408687cec70c5f319f3bc6a7a869ffab20fdd6
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-13 20:17:50 +0530

    Update 2_Some_More_String_methods.java

--- 2_Some_More_String_methods.java
+++ 2_Some_More_String_methods.java
@@ -1,7 +1,15 @@
 public class SomeMoreStringMethods {
 
 	public static void main(String[] args) {
+	       String s= "shashwatkumarshashwatkumar";
 		
+		System.out.println(s.indexOf('h'));
+		System.out.println(s.indexOf('h',10));
+		System.out.println(s.indexOf("shw"));
+		System.out.println(s.indexOf("shw",10));
+		System.out.println(s.indexOf("shit"));
+		System.out.println(s.indexOf('x'));
+		System.out.println(s.indexOf("shw",17));
 	}
 
 }
