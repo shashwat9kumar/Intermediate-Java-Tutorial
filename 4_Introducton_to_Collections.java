9d73e90f247e8eb9f9207c3a5c579bc80608517f
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-14 20:21:57 +0530

    Update 4_Introducton_to_Collections.java

--- 4_Introducton_to_Collections.java
+++ 4_Introducton_to_Collections.java
@@ -1,1 +1,45 @@
+import java.util.*;
+public class Introduction_to_Collections {
 
+	public static void main(String[] args) {
+		String[] things1 = {"eggs","lasers","hats","pie"};
+		List<String> list1 = new ArrayList<String>();
+		
+		
+		// add array items to list
+		
+		
+		for(String x : things1)
+		{
+			list1.add(x);
+		}
+		
+		
+		
+		String[] things2 = {"lasers","hats"};
+		List<String> list2 = new ArrayList<String>();
+		
+		
+		
+		for(String x : things2)
+		{
+			list2.add(x);
+		}
+		
+		
+		
+		
+		for(int i=0;i<list1.size();i++)
+		{
+			System.out.print(list1.get(i)+ " ");
+		}
+		System.out.println();
+		for(int i=0;i<list2.size();i++)
+		{
+			System.out.print(list2.get(i)+ " ");
+		}
+		
+		
+	}
+
+}
