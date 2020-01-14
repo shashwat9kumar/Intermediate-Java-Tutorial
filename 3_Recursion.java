ff1e25e1b0d801c9ab8e08a20a4a6c84bb83294b
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-14 20:02:07 +0530

    Update 3_Recursion.java

--- 3_Recursion.java
+++ 3_Recursion.java
@@ -1,1 +1,22 @@
 
+public class Recursion {
+
+	public static void main(String[] args) {
+		
+		System.out.println(fact(20));
+		
+	}
+	
+	
+	
+	//factorial method usin recursion
+	
+	public static long fact(long n)
+	{
+		if(n<=1)
+			return 1;          // base case;
+		else
+			return n*fact(n-1);        // function calls are stored
+	}
+
+}
